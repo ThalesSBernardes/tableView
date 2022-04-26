ceeda25ac9fbab666e40b16f135969e2949be053
Thales de Sousa Bernardes <thales-bernardes@hotmail.com>
2022-04-26 19:31:08 -0300

    Teoria em andamento

---
+++ tableView/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by Thales S. Bernardes on 26/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+}
+    
+
